9bb5b324f8b473b60970df8ab905a8a87dfeb56b
chadsmith12 <8794805+chadsmith12@users.noreply.github.com>
2024-10-20 16:56:09 -0500

    Makefile live reloading

--- Makefile
+++ Makefile
@@ -1,2 +1,25 @@
 live/tailwind:
 	npx tailwindcss -i ./app/assets/css/tailwind.css -o ./wwwroot/styles/styles.css --watch
+
+live/templ:
+	templ generate --watch --proxy="http://localhost:4500" --open-browser=false
+
+live/app:
+	air \
+		--build.cmd "go build -o ./tmp/app ./cmd/web" \
+		--build.exclude_dir "node_modules" \
+		--build.include_ext "go" \
+		--build.stop_on_error "false" \
+		--misc.clean_on_exit true
+
+live/web:
+	air \
+		--build.cmd "templ generate --notify-proxy" \
+		--build.bin "true" \
+		--build.delay "100" \
+		--build.exclude_dir "" \
+		--build.include_dir "app/assets/*, wwwroot" \
+		--build.include_ext "js,css"
+
+live:
+	make -j2 live/templ live/app live/tailwind live/web
--- app/views/page_templ.go
+++ app/views/page_templ.go
@@ -29,20 +29,20 @@ func Hello(name string) templ.Component {
 			templ_7745c5c3_Var1 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html lang=\"en\"><head><title>Home Page</title><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"/styles/styles.css\"></head><body class=\"font-bold underline text-black\">Hello, ")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("<html lang=\"en\"><head><title>Home Page</title><meta charset=\"UTF-8\"><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"stylesheet\" href=\"/styles/styles.css\"></head><body class=\"font-bold\">Hello,  <span class=\"text-green-500\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
 		var templ_7745c5c3_Var2 string
 		templ_7745c5c3_Var2, templ_7745c5c3_Err = templ.JoinStringErrs(name)
 		if templ_7745c5c3_Err != nil {
-			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/views/page.templ`, Line: 12, Col: 25}
+			return templ.Error{Err: templ_7745c5c3_Err, FileName: `app/views/page.templ`, Line: 12, Col: 55}
 		}
 		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var2))
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</body></html>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</span></body></html>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
